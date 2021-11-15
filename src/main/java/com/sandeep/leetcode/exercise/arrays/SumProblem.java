b85ccb8edaf73a266edcd6f6ae0e7e1b526fe945
sandeepkumarshukla <sandeep.shukla@gmail.com>
2021-11-15 14:51:44 +0100

    Multidimensional concept dymestified. WELL DONE.

--- src/main/java/com/sandeep/leetcode/exercise/arrays/SumProblem.java
+++ src/main/java/com/sandeep/leetcode/exercise/arrays/SumProblem.java
@@ -1,15 +1,13 @@
 package com.sandeep.leetcode.exercise.arrays;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class SumProblem {
 
     public static void main(String[] args) {
-        twoSumProblem();
-        threeSumProblem();
+        //twoSumProblem();
+        //threeSumZeroProblem();
+        //twoSumZeroProblem();
     }
 
     private static int[] twoSumProblem() {
@@ -32,9 +30,44 @@ public class SumProblem {
             return new int[] {} ;
     }
 
-    private static void threeSumProblem() {
+    private static void threeSumZeroProblem() {
+        //callicoder
+        int[] nums = {7, 12, 3,1 ,2, -6, 5, -8, 6};
+        int target = 0;
+        List<Integer[]> result = new ArrayList<>();
 
+        for( int i=0; i< nums.length ;i++) {
+            for( int j = i+ 1; j< nums.length ;j++) {
+                for ( int k = j+1 ; k < nums.length ;k++) {
+                    if(nums[i] + nums[j] + nums[k] == target) {
+                        Integer[] numArr = { nums[i] , nums[j] ,nums[k] };
+                        result.add(numArr);
+                    }
+                }
+            }
+        }
+        for(Integer[] intval : result) {
+            System.out.println("The value is :"+ Arrays.deepToString(intval));
+        }
+    }
+    //generic method to resolve the issue.
+    private static void twoSumZeroProblem() {
+        //callicoder
+        int[] nums = {7, 12, 3,1 ,2, -6, 5, -8, 6};
+        int target = 19;
+        List<Integer[]> result = new ArrayList<>();
 
+        for( int i=0; i< nums.length ;i++) {
+            for( int j = i+ 1; j< nums.length ;j++) {
+                    if(nums[i] + nums[j] == target) {
+                        Integer[] numArr = { nums[i] , nums[j]  };
+                        result.add(numArr);
+                    }
 
+            }
+        }
+        for(Integer[] intval : result) {
+            System.out.println("The value is :"+ Arrays.deepToString(intval));
+        }
     }
 }
--- src/main/java/com/sandeep/leetcode/personal/arrays/PersonalConceptArrays.java
+++ src/main/java/com/sandeep/leetcode/personal/arrays/PersonalConceptArrays.java
@@ -3,5 +3,68 @@ package com.sandeep.leetcode.personal.arrays;
 public class PersonalConceptArrays {
     public static void main(String[] args) {
 
+        //loopTwoDimentionalArray();
+        loopThreeDimensionalArray();
+        //loopThreeDimensionalArray2();
+    }
+
+    private static void loopThreeDimensionalArray2() {
+        int[][][] arr={
+                {
+                        {34,67,43},
+                        {576,697,423},
+                        {576,587,90}
+                },
+                {
+                        {39,47,33},
+                        {376,987,453},
+                        {57,69,42}
+                },
+        };
+        for(int i=0; i<=0; i++){
+            for(int j=0; j<=0; j++){
+                for(int k=0; k<=2; k++){
+                    System.out.println(arr[i][j][k]);
+                }
+            }
+        }
+    }
+
+    private static void loopThreeDimensionalArray() {
+        int[][][] arr={
+                {
+                        {34,67,43},
+                        {576,697,423},
+                        {576,587,90}
+                },
+                {
+                        {39,47,33},
+                        {376,987,453},
+                        {57,69,42}
+                },
+        };
+        for(int i =0; i<arr[0][0].length; i++) {
+            System.out.println("The values are :"+arr[0][1][i]);
+            for(int j= 0 ; j<arr[0][0].length; j++) {
+               // System.out.println("The values are :"+arr[0][0][j]);
+                for(int k =0 ; k<arr[0][0].length ; k++) {
+
+                }
+
+            }
+        }
+
+    }
+
+    private static void loopTwoDimentionalArray() {
+        int[][] a = {
+                {1, 2, 3},
+                {4, 5, 6, 9},
+                {7},
+        };
+
+        for(int i =0; i<a[0].length ; i++) {
+            System.out.println("The values are :"+a[0][i]);
+        }
     }
 }
